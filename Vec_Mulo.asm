57f4415ee2645039708604ca902ac5377a9b6c2f
keithfarnham <keith.farnham@student.csulb.edu>
2015-04-18 15:30:40 -0700

    more comments

--- Vec_Mulo.asm
+++ Vec_Mulo.asm
@@ -27,10 +27,10 @@ main:      #         ***** Your code begins here *****
 
 	   la $t0, array_1 				#load t0 with memory location 0x10010000 to store vec a
 	   add $t1, $t0, 32 				#load t1 with memory location 0x10010020 to store vec b
-	   li $a0, 0x233C475D				#initialize a0 with upper bits of vec a
-	   li $a1, 0x087F196F				#initialize a1 with lower bits of vec a
-	   li $a2, 0x981963C5				#initialize a2 with upper bits of vec b
-	   li $a3, 0x5E80B36E				#initialize a3 with lower bits of vec b
+	   li $a0, 0xAEE95AE0				#initialize a0 with upper bits of vec a
+	   li $a1, 0xF080CC66				#initialize a1 with lower bits of vec a
+	   li $a2, 0x33146170				#initialize a2 with upper bits of vec b
+	   li $a3, 0x609888AB				#initialize a3 with lower bits of vec b
 	   srl $t8, $a0, 24				#shift to clear bits and store first 8 bits of upper vec a into t5
 	   sw $t8, 0($t0)				#store first 8 bits of upper vec a into memory
 	   sll $t8, $a0, 8				#shift to clear bits higher than second 8 bits of upper vec a
@@ -79,31 +79,25 @@ main:      #         ***** Your code begins here *****
 	   srl $t8, $t8, 24				#shift to clear bits and store last 8 bits of lower vec b into t8
 	   sw $t8, 28($t1)				#store last 8 bits of lower vec b into memory	
 	   
-	   lw $t2, 4($t0)  		#load array slot 0 of $t0 into $t2
-	   lw $t3, 4($t1)		#load array slot 0 of $t1 into $t3
-	
-	   mul $t5, $t2, $t3		#multiply values from array
-	   sll $t5, $t5, 16		#shift value to first 4 bytes
+	   lw $t2, 4($t0)  				#load first even 8 bits of vec a into t2
+	   lw $t3, 4($t1)				#load first even 8 bits of vec b into t3
+	   mul $t5, $t2, $t3				#multiply first even 8 bits of a and b and store into t5
+	   sll $t5, $t5, 16				#shift result to first 16 bits
 	   
-	   lw $t2, 12($t0) 		#load array slot 2 of $t0 into $t2
-	   lw $t3, 12($t1)		#load array slot 2 of $t1 into $t3
-	   mul $t7, $t2, $t3		#multiply values from array and store in temp reg
-	   add $t5, $t5, $t7		#move new values from temp reg to $t5
+	   lw $t2, 12($t0) 				#load second even 8 bits of vec a into t2
+	   lw $t3, 12($t1)				#load second even 8 bits of vec b into t2
+	   mul $t7, $t2, $t3				#multiply second even 8 bits of a and b and store into t5
+	   add $t5, $t5, $t7				#move product from temp reg to t5
 	   
-	   lw $t2, 20($t0)  		#load array slot 0 of $t0 into $t2
-	   lw $t3, 20($t1)		#load array slot 0 of $t1 into $t3
-	
-	   mul $t6, $t2, $t3		
-	   sll $t6, $t6, 16		
+	   lw $t2, 20($t0)  				#load third even 8 bits of vec a into t2
+	   lw $t3, 20($t1)				#load third even 8 bits of vec a into t2
+	   mul $t6, $t2, $t3				#multiply third even 8 bits of a and b and store into t6
+	   sll $t6, $t6, 16				
 	   
-	   lw $t2, 28($t0) 
-	   lw $t3, 28($t1)	
-	   mul $t7, $t2, $t3
-	   add $t6, $t6, $t7
-	   
-	 	
-	   
-	   #LOWER BITS SAVED INTO $t5 UPPER BITS SAVED INTO $t6
+	   lw $t2, 28($t0) 				#load last even 8 bits of vec a into t2
+	   lw $t3, 28($t1)				#load last even 8 bits of vec a into t2
+	   mul $t7, $t2, $t3				#multiply last even 8 bits of a and b and store into t5
+	   add $t6, $t6, $t7				#move product from temp reg to t6
 
            #-----------------------------------------------------------
            # "Due diligence" to return control to the kernel
