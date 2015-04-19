bf7f13e206c7ec87387471e8b8a125321ff80915
keithfarnham <keith.farnham@student.csulb.edu>
2015-04-18 17:56:59 -0700

    added comments for first part

--- Vec_compare_Equal_To.asm
+++ Vec_compare_Equal_To.asm
@@ -23,62 +23,60 @@
            .text                        # main (must be global)
            .globl main
 
-main:      #         ***** Your code begins here *****
-
-	    la $t0, array_1 #initialize $t1 to point to memory location with label "array_1"
-	   add $t1, $t0, 32 #initialize $t0 to point to memory location with label "array_2"
-	   li $a0, 0x5AFB6C1D
-	   li $a1, 0xA65FC040
-	   li $a2, 0x52FBA415
-	   li $a3, 0xAE5FC841
-	   srl $t8, $a0, 24
-	   sw $t8, 0($t0)
-	   sll $t8, $a0, 8
-	   srl $t8, $t8, 24
-	   sw $t8, 4($t0)
-	   sll $t8, $a0, 16
-	   srl $t8, $t8, 24
-	   sw $t8, 8($t0)
-	   sll $t8, $a0, 24
-	   srl $t8, $t8, 24
-	   sw $t8, 12($t0)
+main:      
+	   la $t0, array_1 				#load t0 with memory location 0x10010000 to store vec a
+	   add $t1, $t0, 32 				#load t1 with memory location 0x10010020 to store vec b
+	   li $a0, 0x5AFB6C1D				#initialize a0 with upper bits of vec a
+	   li $a1, 0xA65FC040				#initialize a1 with lower bits of vec a
+	   li $a2, 0x52FBA415				#initialize a0 with upper bits of vec b
+	   li $a3, 0xAE5FC841				#initialize a1 with lower bits of vec b
+	   srl $t8, $a0, 24				#shift to clear bits and store first 8 bits of upper vec a into t5
+	   sw $t8, 0($t0)				#store first 8 bits of upper vec a into memory
+	   sll $t8, $a0, 8				#shift to clear bits higher than second 8 bits of upper vec a
+	   srl $t8, $t8, 24				#shift to clear bits and store second 8 bits upper of vec a into t8
+	   sw $t8, 4($t0)				#store second 8 bits of uppper vec a into memory
+	   sll $t8, $a0, 16				#shift to clear bits higher than third 8 bits of upper vec a
+	   srl $t8, $t8, 24				#shift to clear bits and store third 8 bits of upper vec a into t8
+	   sw $t8, 8($t0)				#store third 8 bits of upper vec a into memory
+	   sll $t8, $a0, 24				#shift to clear bits higher than last 8 bits of upper vec a
+	   srl $t8, $t8, 24				#shift to clear bits and store last 8 bits of upper vec a into t8
+	   sw $t8, 12($t0)				#store last 8 bits of upper vec a into memory
 	   
-	   srl $t8, $a1, 24
-	   sw $t8, 16($t0)
-	   sll $t8, $a1, 8
-	   srl $t8, $t8, 24
-	   sw $t8, 20($t0)
-	   sll $t8, $a1, 16
-	   srl $t8, $t8, 24
-	   sw $t8, 24($t0)
-	   sll $t8, $a1, 24
-	   srl $t8, $t8, 24
-	   sw $t8, 28($t0)
+	   srl $t8, $a1, 24				#shift to clear bits and store first 8 bits of lower vec a into t8
+	   sw $t8, 16($t0)				#store first 8 bits of lower vec a into memory
+	   sll $t8, $a1, 8				#shift to clear bits higher than second 8 bits of lower vec a
+	   srl $t8, $t8, 24				#shift to clear bits and store second 8 bits of lower vec a into t8
+	   sw $t8, 20($t0)				#store second 8 bits of lower vec a into memory
+	   sll $t8, $a1, 16				#shift to clear bits higher than third 8 bits of lower vec a
+	   srl $t8, $t8, 24				#shift to clear bits and store third 8 bits of lower vec a into t8
+	   sw $t8, 24($t0)				#store third 8 bits of lower vec a into memory
+	   sll $t8, $a1, 24				#shift to clear bits higher than last 8 bits of lower vec a
+	   srl $t8, $t8, 24				#shift to clear bits and store last 8 bits of lower vec a into t8
+	   sw $t8, 28($t0)				#store last 8 bits of lower vec a into memory
 	   
-	   srl $t8, $a2, 24
-	   sw $t8, 0($t1)
-	   sll $t8, $a2, 8
-	   srl $t8, $t8, 24
-	   sw $t8, 4($t1)
-	   sll $t8, $a2, 16
-	   srl $t8, $t8, 24
-	   sw $t8, 8($t1)
-	   sll $t8, $a2, 24
-	   srl $t8, $t8, 24
-	   sw $t8, 12($t1)
-	   
-	   srl $t8, $a3, 24
-	   sw $t8, 16($t1)
-	   sll $t8, $a3, 8
-	   srl $t8, $t8, 24
-	   sw $t8, 20($t1)
-	   sll $t8, $a3, 16
-	   srl $t8, $t8, 24
-	   sw $t8, 24($t1)
-	   sll $t8, $a3, 24
-	   srl $t8, $t8, 24
-	   sw $t8, 28($t1)
-	   
+	   srl $t8, $a2, 24				#shift to clear bits and store first 8 bits of upper vec b into t8
+	   sw $t8, 0($t1)				#store first 8 bits of upper vec b into memory
+	   sll $t8, $a2, 8				#shift to clear bits higher than second 8 bits of upper vec b
+	   srl $t8, $t8, 24				#shift to clear bits and store second 8 bits upper of vec b into t8
+	   sw $t8, 4($t1)				#store second 8 bits of uppper vec b into memory
+	   sll $t8, $a2, 16				#shift to clear bits higher than second 8 bits of upper vec b
+	   srl $t8, $t8, 24				#shift to clear bits and store second 8 bits upper of vec b into t8
+	   sw $t8, 8($t1)				#store third 8 bits of uppper vec b into memory
+	   sll $t8, $a2, 24				#shift to clear bits higher than second 8 bits of upper vec b	
+	   srl $t8, $t8, 24				#shift to clear bits and store second 8 bits of upper vec b into t8
+	   sw $t8, 12($t1)				#store last 8 bits of uppper vec b into memory
+	   						
+	   srl $t8, $a3, 24				#shift to clear bits and store first 8 bits of lower vec b into t8	
+	   sw $t8, 16($t1)				#store first 8 bits of lower vec b into memory
+	   sll $t8, $a3, 8				#shift to clear bits higher than second 8 bits of lower vec b
+	   srl $t8, $t8, 24				#shift to clear bits and store second 8 bits lower of vec b into t8
+	   sw $t8, 20($t1)				#store second 8 bits of lower vec b into memory	
+	   sll $t8, $a3, 16				#shift to clear bits higher than third 8 bits of lower vec b
+	   srl $t8, $t8, 24				#shift to clear bits and store third 8 bits lower of vec b into t8
+	   sw $t8, 24($t1)				#store third 8 bits of lower vec b into memory	
+	   sll $t8, $a3, 24				#shift to clear bits higher than last 8 bits of lower vec b
+	   srl $t8, $t8, 24				#shift to clear bits and store last 8 bits of lower vec b into t8
+	   sw $t8, 28($t1)				#store last 8 bits of lower vec b into memory	
 	   
 	   lw $t2, 0($t0) 		#load array slot 0 of $t0 into $t2
 	   lw $t3, 0($t1)		#load array slot 0 of $t1 into $t3
